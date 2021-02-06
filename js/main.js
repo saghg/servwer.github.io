4c3580c8567ad27c3d2b1048a2a121c6862b8602
servwer <iviris@bk.ru>
2021-02-06 22:10:47 +0300

    add modal window to "header section"

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
 $(function(){
-
+	$('.header__inner-btn button').on('click',function () {
+		$('.modal__header').removeClass('hidden').fadeIn();
+		// alert('click');
+	});
+	$('.close__btn').on('click', function (){
+		$('.modal__header').addClass('hidden').fadeOut();
+	});
 });
