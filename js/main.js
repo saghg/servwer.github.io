994c192a8587c399e9ab593b57f8fe825b9cce22
servwer <iviris@bk.ru>
2021-02-06 22:12:18 +0300

    as

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
-$(function(){
-	$('.header__inner-btn button').on('click',function () {
+$(function () {
+	$('.header__inner-btn button').on('click', function () {
 		$('.modal__header').removeClass('hidden').fadeIn();
 		// alert('click');
 	});
-	$('.close__btn').on('click', function (){
+	$('.close__btn').on('click', function () {
 		$('.modal__header').addClass('hidden').fadeOut();
 	});
 });
