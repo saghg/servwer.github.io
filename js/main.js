0ef173f75f75e82ed47a4dc8c69e71cf3c1c1679
servwer <iviris@bk.ru>
2021-02-07 17:45:32 +0300

    add up button and fixed some things

--- js/main.js
+++ js/main.js
@@ -1,8 +1,43 @@
 $(function () {
 	$('.header__inner-btn button').on('click', function () {
 		$('.modal__header').removeClass('hidden').fadeIn();
+		$('body').css('overflow','hidden');
 	});
 	$('.close__btn').on('click', function () {
 		$('.modal__header').addClass('hidden').fadeOut();
+		$('body').css('overflow', 'auto');
+	});
+
+
+	$(".nav").on("click", "a", function (event) {
+		event.preventDefault();
+		var id = $(this).attr('href'),
+			top = $(id).offset().top;
+		$('body,html').animate({ scrollTop: top }, 1000);
+	});
+	$(".arrow__up").on("click", "a", function (event) {
+		event.preventDefault();
+		var id = $(this).attr('href'),
+			top = $(id).offset().top;
+		$('body,html').animate({ scrollTop: top }, 1000);
+	});
+
+
+
+	$("#back-top").hide();
+
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > 700) {
+			$("#back-top").fadeIn();
+		} else {
+			$("#back-top").fadeOut();
+		}
+	});
+
+	$("#back-top a").click(function () {
+		$("body,html").animate({
+			scrollTop: 0
+		}, 800);
+		return false;
 	});
 });
